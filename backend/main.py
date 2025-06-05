3b0983d18f72e73a826a6d990b023c33f20389a1
alinaparhom <alinych.parhom@gmail.com>
2025-06-05 16:12:43 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=".*",
-    allow_credentials=True,
+    allow_origins=["*"],  # разрешаем доступ Telegram и любым клиентам
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/userdata")
-def get_user_data():
-    return {
-        "name": "Иван",
-        "balance": 1000,
-        "premium": False
-    }
+# путь к папке, где хранятся модели
+MODEL_DIR = os.path.join(os.path.dirname(__file__), "models")
+
+@app.get("/model/{filename}")
+async def get_model(filename: str):
+    file_path = os.path.join(MODEL_DIR, filename)
+    if os.path.exists(file_path):
+        return FileResponse(file_path, media_type="model/gltf+json")
+    return {"error": "File not found"}
